026deabb5103d130b26520fe5a8ded7e282fd132
Kalenda Martin <kalenda@oksystem.cz>
2024-09-26 16:33:08 +0200

    revert ReleaseDashboardApplicationTests.java

--- backend/deployment-dashboard/src/test/java/cz/oksystem/deployment_dashboard/DeploymentDashboardApplicationTests.java
+++ backend/deployment-dashboard/src/test/java/cz/oksystem/deployment_dashboard/DeploymentDashboardApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ReleaseDashboardApplicationTests {
+class DeploymentDashboardApplicationTests {
 
 	@Test
 	void contextLoads() {
